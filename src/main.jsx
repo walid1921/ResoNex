78b7887f12e9f7347c10bb90034cfe8438f56fe6
user.walid1921 <user.kouiderayadwalid@gmail.com>
2023-11-29 21:30:30 +0200

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <video autoPlay muted loop id="video-background">
-      <source src="https://assets.codepen.io/3364143/7btrrd.mp4" type="video/mp4" />
+    {/* <video autoPlay muted loop id="video-background">
+      <source src="./backgroundVideo.mp4" type="video/mp4" />
       Your browser does not support the video tag.
-    </video>
-    
+    </video> */}
+
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   </>
 )
