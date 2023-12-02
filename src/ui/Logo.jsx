c8dbdd2bb3c1dfd5442b3ec9184800b6f2f432ee
user.walid1921 <user.kouiderayadwalid@gmail.com>
2023-12-02 18:39:02 +0200

    logoUpdated

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 
 const Logo = () => {
   return (
-    <div className='flex items-center justify-center px-10 py-8 border-b border-slate-600  '>   
+    <div className='flex items-center justify-center px-14 py-8 border-b border-slate-600  '>   
       <Link to='/'>
         {/* <span className='text-3xl font-semibold text-transparent bg-clip-text bg-gradient-to-br from-[#2660f3] to-[#41596d]'>ResoNex</span> */}
-        <img src="logoIcon.png" alt="logoIcon" />
+        <img src="logo.png" alt="logoIcon"/>
         
         </Link>
       </div>
